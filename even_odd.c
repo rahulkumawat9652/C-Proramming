e3dc70f5266c2340ba4004c3f659b07f93fd6c49
Rahul kumawat <rahulkumawat9652@gmail.com>
2025-10-27 20:45:49 +0530

    all file added

--- even_odd.c
+++ even_odd.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-    int n ;
-    printf("Enter the number : ");
-    scanf("%d",&n);
-    if ( n % 2 == 0)
-        printf("%d is even ",n);
+    int a,b;
+    printf("enter two number ");
+    scanf("%d%d",&a,&b);
+    if (a>b)
+       printf("a is maxium, %d\n");
     else
-        printf("%d is odd.",n);
+       printf("b is maxium,%d\n");
+    
     return 0;
 }
