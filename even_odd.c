0382c0d7cee496cf5df7409818057c2a7a6ea026
rahulkumawat9652 <rahulkumawat9652@gmail.com>
2025-10-29 16:11:06 +0530

    Update even_odd.c

--- even_odd.c
+++ even_odd.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main(){
+    //evan -> 1
+    //odd -> 0
+    int n;
+    printf("enter a number ");
+    scanf("%d",&n);
+    printf("%d",n % 2== 0);    
+    return 0;
+}
